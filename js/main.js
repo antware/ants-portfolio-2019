bedbc9667efe946380ba647a55c01123e43b8e36
Antony Ware <antony@warcom.co.za>
2019-08-01 10:46:16 +0200

    Uploading almost completed portfolio

--- js/main.js
+++ js/main.js
@@ -15,5 +15,26 @@ let menuItems = document.querySelectorAll('.nav-link');
 menuItems.forEach((menuItem) => {
   menuItem.addEventListener('click', () => {
     menu.classList.toggle('slide');
+    animateBurger(burgerTrigger);
   })
 })
+
+// jQuery Scripts for ScrollMagic and GSAP
+$(document).ready(function () {
+  // INit Scrollmagic
+  let controller = new ScrollMagic.Controller();
+
+  // loop through each skill
+  $('.skills').each(function () {
+    // build a scene
+    let ourScene = new ScrollMagic.Scene({
+      triggerElement: /*this.children[0]*/'.skill-line',
+      reverse: false
+    })
+      .setClassToggle(this, 'slide') // add class to project01
+      .addTo(controller);
+  });
+
+  console.log(this.children);
+});
+
